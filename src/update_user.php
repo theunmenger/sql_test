3efe77fcdfa95044eabb18d9917a373a717bf064
theunmenger <theun2005@hotmail.com>
2025-06-28 14:58:11 +0200

    changed way of handling updates

--- src/update_user.php
+++ src/update_user.php
@@ -8,54 +8,11 @@
 
     $conn = require_once "db_connect.php";
 
-    $username_err = $email_err = $update_notif = "";
-
     $user = $email = "";
-
 
     if ($_SERVER["REQUEST_METHOD"] == "POST" && !isset($_POST['update']) && isset($_POST['username']) && isset($_POST['email'])) {
         $user = $_POST['username'];
         $email = $_POST['email'];
-    }
-    
-    
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update'])) {
-        $user = $_POST['username'];
-        $email = $_POST['email'];
-        $new_username = $_POST['new_username'];
-        $new_email = $_POST['new_email'];
-        $admin = $_POST['admin_status'];
-
-        $email_ver = false;
-        $name_ver = false;
-        
-        if(!filter_var($new_email, FILTER_VALIDATE_EMAIL)) {
-            $email_err = "Invalid email";
-        } else {
-            $email_ver = true;
-        }
-
-        if (!preg_match("/^[a-zA-Z-0-9_]+$/", $user)) {
-            $username_err = "Username can only contain letters, numbers, underscores (_), and hyphens (-)";
-        } else {
-            $name_ver = true;
-        }
-            
-        if ($name_ver && $email_ver) {
-            echo "new_user: ". $new_username. " new_email: ". $new_email. " old_user: ". $user. " old_email: ". $email. " admin status: ". $admin;
-        }
-
-        if (!empty($_POST['new_username'])) {
-            $update_username = "update inlog_gegevens_table set username = '$new_username' where email = '$email'";
-        }
-
-        if (!empty($_POST['new_email'])) {
-            $update_email = "update inlog_gegevens_table set email = '$new_email' where email = '$email'";
-        }
-
-        if (!empty($_POST['admin_status'])) {
-            $update_admin = "update inlog_gegevens_table set admin = '$admin' where email = '$email'";
-        }
     }
 ?>
 
@@ -71,23 +28,29 @@
     <div class="container_top">
         <h2>Update user: <?php echo $user?></h2>
         <a href="logged_in.php">Back</a>
-        <p><?php echo $update_notif;?></p>
-        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-            <input type="hidden" name="username" value="<?php echo htmlspecialchars($user); ?>">
-            <input type="hidden" name="email" value="<?php echo htmlspecialchars($email); ?>">
-            <label for="new_email">New mail:</label><br>
-            <input class="input" type="text" id="new_email" name="new_email" placeholder="Example@gmail.com...">
-            <p class="error"><?php echo $email_err;?></p> <br>
-            <label for="new_username">New username:</label><br>
-            <input class="input" type="text" id="new_username" name="new_username" placeholder="Username..."> 
-            <p class="error"><?php echo $username_err;?></p><br>
-            <p>Admin status</p>
-            <input type="radio" id="yes" name="admin_status" value="1">
-            <label for="yes">Yes</label><br>
-            <input type="radio" id="no" name="admin_status" value="0">
-            <label for="no">No</label><br><br>
-            <input class="input" type="submit" name="update" value="Update">
-        </form>
+        <div id="form_container">
+            <form method="POST" action="change_name.php" style="display:inline;">
+                <input type="hidden" name="email" value="<?php echo htmlspecialchars($email);?>">
+                <input class="input" type="text" id="username" name="username" placeholder="New username..." minlength="3"> 
+                <button class="input_b" type="submit">Update username</button>
+            </form>
+
+            <form method="POST" action="change_email.php" style="display:inline;">
+                <input type="hidden" name="email" value="<?php echo htmlspecialchars($email);?>">
+                <input class="input" type="text" id="email" name="email" placeholder="Example@gmail.com...">
+                <button class="input_b" type="submit">Update email</button>
+            </form><br><br>
+            
+            <form method="POST" action="change_admin.php" style="display:inline;">
+                <p>Has admin:</p>
+                <input type="hidden" name="email" value="<?php echo htmlspecialchars($email);?>">
+                <input type="radio" name="admin_status" id="yes" value="1">
+                <label for="yes">Yes</label><br>
+                <input type="radio" name="admin_status" id="no" value="0">
+                <label for="yes">No</label><br><br>
+                <button class="input_b" type="submit">Update admin status</button>
+            </form>
+        </div>
     </div>
 </body>
 </html>
